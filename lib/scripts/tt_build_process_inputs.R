efe026766619696e4fc58caf69afc37cb3fa2bb7
Colin Smith <colin76@gmail.com>
2022-09-21 10:30:03 -0400

    updates to trip tables script to produce 27 trips tables -- daily totals by vehicle and 3 * 8 time period trip tables

--- lib/scripts/tt_build_process_inputs.R
+++ lib/scripts/tt_build_process_inputs.R
@@ -7,6 +7,11 @@ tt_build_process_inputs <- function(envir){
   ### Load inputs/outputs from earlier steps
   load(file.path(SCENARIO_OUTPUT_PATH, SYSTEM_CVTM_OUTPUTNAME))
   
-  return(cv_cv_sim_results$cv_trips)
+  # extract the lookup from the skims for use in the trip tables
+  envir[["zone_number"]] <- readLookupOMX(file.path(SCENARIO_INPUT_PATH, 
+                                                    cv_inputs$skims_names[1]$file_name), 
+                                          LookupName = "zone_number")$Lookup
+  
+  return(cv_sim_results$cv_trips)
   
 }
