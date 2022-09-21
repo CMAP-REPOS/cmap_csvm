c1f5ffe17b8d3148315bd94094e36e6621c21acd
Colin Smith <colin76@gmail.com>
2022-09-21 10:30:03 -0400

    updates to trip tables script to produce 27 trips tables -- daily totals by vehicle and 3 * 8 time period trip tables #29

--- lib/scripts/tt_build.R
+++ lib/scripts/tt_build.R
@@ -1,69 +1,62 @@
 # Converts the trip list to a trip table
-tt_build <- function(shipments) {
+tt_build <- function(cv_trips) {
   
   # Begin progress tracking
   progressStart(action = "Writing...", task = "Trip Tables", dir = SCENARIO_LOG_PATH, subtasks = FALSE)
   
-  setkey(shipments,Day_ID,Tour_ID2,Trip_Seq)
+  ### Aggregate cv_trips up to a trip table and write results as a set of OMX matrices
+  cat("Aggregating Commercial Vehicle Tours to OD Trips", "\n")
   
-  ### -- Heither, 05-10-2016: assign trip to CMAP TOD category
-  shipments[StartTime>=20 | StartTime< 6, CMAP_TOD:=1]
-  shipments[StartTime>= 6 & StartTime< 7, CMAP_TOD:=2]
-  shipments[StartTime>= 7 & StartTime< 9, CMAP_TOD:=3]
-  shipments[StartTime>= 9 & StartTime<10, CMAP_TOD:=4]
-  shipments[StartTime>=10 & StartTime<14, CMAP_TOD:=5]
-  shipments[StartTime>=14 & StartTime<16, CMAP_TOD:=6]
-  shipments[StartTime>=16 & StartTime<18, CMAP_TOD:=7]
-  shipments[StartTime>=18 & StartTime<20, CMAP_TOD:=8]
+  TripTable <- cv_trips[, .(trips = .N),
+                       by = .(OTAZ, DTAZ, Vehicle, TOD)]
   
-  ## check StartTimes w/in 24 hour period - esp. Direct return-to-warehouse trips
+  setkeyv(TripTable, c("OTAZ", "DTAZ", "Vehicle", "TOD"))
   
-  ### -- Create Trip Table Storage Location -- 
-  dir.create(paste0(SCENARIO_OUTPUT_PATH,"/trip_tables",sep=""), showWarnings=F)
+  # Create a table of matrix names and types consistent with the CMAP model
+  # Currently there are commercial vehicle trip tables for each combination of
+  # light, medium, heavy trucks and the eight time periods plus daily totals, for 27 tables
+  vehicletypes <- c("Light", "Medium", "Heavy")
+  todlabels <- names(BASE_TOD_RANGES)
   
-  shipments[,Count:=1]
-  alltrip <- shipments[,list(trips=sum(Count)),by=list(o,d,CMAP_TOD,Veh_Type)]
-  for(i in 1:max(alltrip$Veh_Type))
-  {
-  	tempveh <- alltrip[Veh_Type==i,list(o,d,CMAP_TOD,trips)]
-  	j <- 1
-  	while (j <= max(tempveh$CMAP_TOD))
-  	{
-  		tod <- tempveh[CMAP_TOD==j,list(o,d,CMAP_TOD,trips)]
-  		tod[,CMAP_TOD:=NA]
-  		setkey(tod,o,d)
-  		if(i==1)
-  		{
-  			outmtx <- paste0(SCENARIO_OUTPUT_PATH,"/trip_tables/l_tod_",j,".txt",sep="")
-  			cat("t matrices\nd matrix=mf15\na matrix=mf15 ltrk 0 light truck trips for TOD", j,"\n", file=outmtx)
-  		}
-  		else if(i==2)
-  		{
-  			outmtx <- paste0(SCENARIO_OUTPUT_PATH,"/trip_tables/m_tod_",j,".txt",sep="")
-  			cat("t matrices\nd matrix=mf16\na matrix=mf16 mtrk 0 medium truck trips for TOD", j,"\n", file=outmtx)
-  		}
-  		else
-  		{
-  			outmtx <- paste0(SCENARIO_OUTPUT_PATH,"/trip_tables/h_tod_",j,".txt",sep="")
-  			cat("t matrices\nd matrix=mf17\na matrix=mf17 htrk 0 heavy truck trips for TOD", j,"\n", file=outmtx)
-  		}
-  		write.table(tod, file=outmtx, append=T, sep=" ", na=":", row.names=F, col.names=F)
-  		j <- j+1
-  	}
+  matnames <- paste(rep(c(todlabels, "Daily"), length(vehicletypes)),
+                    rep(vehicletypes, each = length(todlabels)+1),
+                    "Truck")
   
-  }
+  outputMatrices <- data.table(matname = matnames,
+                               vehicletype = rep(vehicletypes, each = length(todlabels)+1),
+                               tod = rep(c(todlabels, "all"), length(vehicletypes)))
   
-  #Assigning a text Time of Day category 
-  shipments[,TimeOfDay:=c("EM","AM","MD","PM","NT")[1+findInterval(StartTime,c(6,10,15,19))]]
-  #Assigning a text Vehicle type
-  shipments[,Vehicle:=c("2-axle","3,4 axle","semi/trailer")[Veh_Type]]
-  #Aggregate by zone
-  trip_table <- shipments[,list(Trips=.N),by=list(ORI_mz=o, DES_mz=d,Vehicle,TimeOfDay)]
-
+  outputMatrices[, matrix_description := paste(SCENARIO_NAME, tod, vehicletype, "CV matrix")]
+  
+  outputMatrices[, omxoutputpath := rep(c(file.path(SCENARIO_ASSIGN_PATH, paste0("OD_", c(todlabels, "DY"), ".OMX"))), length(vehicletypes))]
+  
+  outputMatrices[, matname_short := paste(rep(vehicletypes, each = length(todlabels)+1),"Truck")]
+  
+  # Write the CV trip tables to an OMX file in the scenario output folder
+  cat("Write Commercial Vehicle Trips Tables to OMX File", "\n")
+    
+  write_triptables_to_omx(omxoutputpath = rep(file.path(SCENARIO_OUTPUT_PATH, "CV_Trip_Tables.omx"),
+                                                nrow(outputMatrices)),
+                            tazids = c(BASE_TAZ_MODEL_REGION, BASE_TAZ_EXTERNAL),
+                            triptable = TripTable,
+                            matnames = outputMatrices$matname,
+                            matdescriptions = outputMatrices$matrix_description,
+                            dim1name = "TOD",
+                            dim2name = "Vehicle",
+                            dim1 = outputMatrices$tod,
+                            dim2 = outputMatrices$vehicletype)
+  
+  # Add the TripTable to the matrix list for saving
+  tt_list[["TripTable"]] <- TripTable
+  
   # End progress tracking
   progressEnd(dir = SCENARIO_LOG_PATH)
   
-  return(tt_list = list(shipments = shipments, trip_table = trip_table))
+  return(tt_list)
+  
+  # End progress tracking
+  progressEnd(dir = SCENARIO_LOG_PATH)
+  
 }
 
 
