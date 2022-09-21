43595858cf36ce803eda66d8d7766f69786a88ae
Colin Smith <colin76@gmail.com>
2022-09-21 08:08:03 -0400

    updated travel time definitions #18

--- lib/scripts/_BASE_VARIABLES.R
+++ lib/scripts/_BASE_VARIABLES.R
@@ -9,16 +9,24 @@ BASE_SCENARIO_YEARS <- c(2015, 2020, 2025, 2030, 2035, 2040, 2045)
 BASE_SEED_VALUE  <- 5 #seed for sampling to ensure repeatable results
 BASE_TIME_PERIOD_TRIP_POINT <- "START" #point in trip for time period allocation, from ("START", "MIDDLE", "END")
 BASE_TOLL_SKIM_AVAILABLE <- TRUE
+BASE_SKIM_CONDITION <- 'congested' #alternative is 'free flow'
+
 # 3. Define time-of-day groupings for skims and trip tables
 # Units are minutes after midnight 
-# (12:00 AM = 0 at the start of a range, 1440 at the end of a range)
-# AM (6:30 AM - 8:59 AM) 
-# MD (9:00 AM - 2:29 PM)
-# PM (2:30 PM - 6:29 PM)
-# EV (6:30 PM - 9:59 PM) 
-# NT (10:00 PM - 6:29 AM)
-BASE_TOD_RANGES <- list(NT = list(c(0, 390), c(1320, 1440)), 
-                        AM = list(c(390, 540)), 
-                        MD = list(c(540, 870)),
-                        PM = list(c(870, 1110)),
-                        EV = list(c(1110, 1320)))
+# p1 - 8 pm to 6 am
+# p2 - 6 am to 7 am
+# p3 - 7 am to 9 am
+# p4 - 9 am to 10 am
+# p5 - 10 am to 2 pm
+# p6 - 2 pm to 4 pm
+# p7 - 4 pm to 6 pm
+# p8 - 6 pm to 8 pm
+BASE_TOD_RANGES <- list(P1 = list(c(0,360), c(1200,1440)),
+                        P2 = list(c(360,420)),
+                        P3 = list(c(420,540)),
+                        P4 = list(c(540, 600)),
+                        P5 = list(c(600, 840)),
+                        P6 = list(c(840, 960)),
+                        P7 = list(c(960, 1080)),
+                        P8 = list(c(1080, 1200))
+                        )
