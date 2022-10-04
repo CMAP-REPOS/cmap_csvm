dc59273fca7638699381aab0b06f4f58d4db602a
Colin Smith <colin76@gmail.com>
2022-10-04 15:32:19 -0400

    stops models and vehicle model copy

--- dev/estimatIon_control.R
+++ dev/estimatIon_control.R
@@ -9,19 +9,19 @@ source("./dev/init_dev.R")
 ### Open and Review the Estimated Models
 
 # Firm activities
-activities <- readRDS("./dev/Estimation/cv_activities/cv_activities_model_fortesting.RDS")
+activities <- readRDS("./dev/Estimation/cv_activities/cv_activities_model.RDS")
 activities
 
-# # Scheduled Stops
-# goods <- readRDS("./dev/Estimation/cv_stops/final_models/goods/cv_goods_model.RDS")
-# goods$coefficients
-# 
-# service <- readRDS("./dev/Estimation/cv_stops/final_models/service/cv_service_model.RDS")
-# service$coefficients
-# 
-# # Vehicle
-# vehicle <- readRDS("./dev/Estimation/cv_vehicle/cv_vehicle_model.rds")
-# apollo_modelOutput(vehicle)
+# Scheduled Stops
+goods <- readRDS("./dev/Estimation/cv_stops/new_models/goods/cv_goods_model.RDS")
+goods$coefficients
+ 
+service <- readRDS("./dev/Estimation/cv_stops/new_models/services/cv_service_model.RDS")
+service$coefficients
+ 
+# Vehicle
+vehicle <- readRDS("./dev/Estimation/cv_vehicle/final_model/cv_vehicle_model.rds")
+apollo_modelOutput(vehicle)
 # 
 # # Stop Duration
 # stopdur <- readRDS("./dev/Estimation/cv_duration/cv_duration.rds")
@@ -54,10 +54,10 @@ activities
 # saveRDS(activities, "./dev/Estimation/cv_activities/cv_activities_model.RDS")
 
 
-paths_to_models <- file.path("./dev/Estimation", c("cv_activities/cv_activities_model.RDS")) #,
-                                                   # "cv_stops/final_models/goods/cv_goods_model.RDS",
-                                                   # "cv_stops/final_models/service/cv_service_model.RDS",
-                                                   # "cv_vehicle/cv_vehicle_model.rds",
+paths_to_models <- file.path("./dev/Estimation", c("cv_activities/cv_activities_model.RDS",
+                                                   "cv_stops/new_models/goods/cv_goods_model.RDS",
+                                                   "cv_stops/new_models/services/cv_service_model.RDS",
+                                                   "cv_vehicle/final_model/cv_vehicle_model.rds"))
                                                    # "cv_duration/cv_stopduration_model.rds",
                                                    # "cv_tours/cv_tours_model.rds",
                                                    # "cv_arrival/cv_arrival_model.rds",
