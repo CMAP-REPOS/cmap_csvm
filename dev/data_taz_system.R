3d7aea3f13173a312a6f4a21b1e650c83d449d6e
Colin Smith <colin76@gmail.com>
2022-10-18 10:43:26 -0400

    code to simplify the TAZ layer for quicker mapping, roughly halves the size of the dashboard file. #46

--- dev/data_taz_system.R
+++ dev/data_taz_system.R
@@ -7,6 +7,8 @@
 # Outputs:
 # Model inputs in lib\data:
 # TAZ_System.csv
+# TAZ_System_Shape.shp
+# TAZ_System_Shape_Small.shp
 #
 # use init_dev.R to run here instead of sourcing from _Master_Dev.R
 source("./dev/init_dev.R")
@@ -14,7 +16,7 @@ source("./dev/init_dev.R")
 ### READ INPUT FILES ==================================================
 
 emp_control <- fread("./scenarios/base/inputs/data_emp_control_taz.csv")
-zone17 <- read_sf("./lib/data/zones17.shp")
+zone17 <- read_sf(file.path(SYSTEM_DEV_DATA_PATH,"TAZ", "zones17.shp"))
 
 # US Counties
 county <- st_read(file.path(SYSTEM_DEV_DATA_PATH, 
@@ -35,7 +37,7 @@ TAZ_System[, county_state := paste(county, state, sep = ", ")]
 ### CREATE SHAPE FILE VERSION ===================================
 
 TAZ_System_Shape <- st_transform(zone17, crs = st_crs(county))
-TAZ_System_Shape$TAZ <- TAZ_System$zone17
+TAZ_System_Shape$TAZ <- TAZ_System$TAZ
 
 ### SAVE FINAL CORRESPONDENCE ===================================
 
@@ -46,6 +48,28 @@ fwrite(TAZ_System, file.path("lib",
 
 # write the shapefile version 
 st_write(TAZ_System_Shape, 
+         dsn = file.path("lib", 
+                         "data", 
+                         "TAZ_System_Shape.shp"), 
+         delete_layer = TRUE)
+
+# Create a simplified shape file for quicker mapping
+TAZ_System_Shape <- st_read(file.path("lib", "data", "TAZ_System_Shape.shp"))
+plot(TAZ_System_Shape[,c("county_nam")])
+plot(st_simplify(TAZ_System_Shape[,c("county_nam")]
+                 , dTolerance = 100))
+
+TAZ_System_Shape_Small <- st_simplify(TAZ_System_Shape,
+                                      preserveTopology = TRUE,
+                                      dTolerance = 30)
+st_write(TAZ_System_Shape_Small, 
+         dsn = file.path(SYSTEM_DEV_DATA_PATH,
+                         "TAZ", 
+                         "TAZ_System_Shape_Small.shp"), 
+         delete_layer = TRUE)
+
+# overwrite the model version with the small one
+st_write(TAZ_System_Shape_Small, 
          dsn = file.path("lib", 
                          "data", 
                          "TAZ_System_Shape.shp"), 
