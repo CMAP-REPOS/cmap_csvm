9e94438c3b7ac704f2216ecabb751648262371d3
Colin Smith <colin76@gmail.com>
2022-11-01 16:54:33 -0400

    processing future year employment data, covering full range of TAZs #42

--- dev/Process_control_emp.R
+++ dev/Process_control_emp.R
@@ -2,7 +2,7 @@
 library(tidyverse)
 library(readxl)
 library(sf)
-
+# Process Internal-----
 Emp <- read_csv('dev/Data_Processed/Future_Year_Inputs/r211_parcelSum_empsBySZ_2050.csv')
 
 Emp1 <- Emp %>% 
@@ -44,5 +44,60 @@ Emp_Control <- Emp_TAZ_long2 %>%
   filter(!is.na(Zone17))
 
 
-write_csv(Emp_Control, 'scenarios/future/inputs/data_emp_control_taz.csv')
+
+#Process External
+Emp_external <- read_xlsx('dev/Data_Processed/Future_Year_Inputs/xsubzonetm_211_2050.xlsx')
+
+Emp1_external <- Emp_external %>% 
+  select(subzone_id, num_jobs_sector_11:num_jobs_sector_92)
+
+Zone_dict <- st_read('dev/Data_Processed/TAZ/subzones17.shp')
+
+zonedict_1 <- Zone_dict %>%
+  select(subzone17,zone17, county_fip) %>% 
+  st_drop_geometry()
+
+
+
+Emp_TAZ_external <- Emp1_external %>% 
+  left_join(zonedict_1, by = c('subzone_id' = 'subzone17')) %>% 
+  select(zone17, contains('county'), num_jobs_sector_11:num_jobs_sector_92)
+
+Emp_TAZ_grouped_external <- Emp_TAZ_external %>% 
+  group_by(zone17) %>% 
+  summarise(county_fip, across(num_jobs_sector_11:num_jobs_sector_92, ~sum(.))) %>% 
+  distinct()
+
+Emp_TAZ_long_external <- Emp_TAZ_grouped_external %>% 
+  pivot_longer(num_jobs_sector_11:num_jobs_sector_92, names_to = 'NAICS', values_to = 'Employment')
+
+Emp_TAZ_long2_external <- Emp_TAZ_long_external %>% 
+  mutate(NAICS = parse_number(NAICS)) %>% 
+  mutate(NAICS = as.character(NAICS)) %>% 
+  distinct()
+
+Zone_dict <- read_csv('lib/data/TAZ_System.csv') %>% 
+  select(TAZ, Mesozone)
+
+
+Emp_Control_external <- Emp_TAZ_long2_external %>% 
+  left_join(Zone_dict, by = c('zone17' = 'TAZ')) %>% 
+  select(Zone17 = zone17, Mesozone, CountyFIPS = county_fip, NAICS, Employment) %>% 
+  arrange(NAICS, Zone17) %>% 
+  filter(!is.na(Zone17))
+
+
+
+#Bind Interanl and Exteranl
+
+CompleteEmp <- rbind(Emp_Control, Emp_Control_external) %>% 
+  arrange(NAICS, Zone17)
+
+
+
+
+
+
+
+write_csv(CompleteEmp, 'scenarios/future/inputs/data_emp_control_taz.csv')
 
