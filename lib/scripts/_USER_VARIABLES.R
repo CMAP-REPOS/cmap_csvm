3a221fb642f69c46d434475967da0735cbd81fea
Colin Smith <colin76@gmail.com>
2022-11-02 08:27:00 -0400

    removing unused user variables

--- lib/scripts/_USER_VARIABLES.R
+++ lib/scripts/_USER_VARIABLES.R
@@ -1,6 +1,4 @@
 # User options to control outputs, hardware use, and run mode
-USER_GENERATE_DASHBOARD <- TRUE # Should the model generate the output dashboard? (TRUE/FALSE)
-USER_GENERATE_DASHBOARD_MAPS <- TRUE # Should the output dashboard inclde maps? (TRUE/FALSE)
 USER_PROCESSOR_CORES <- 4L # How many processors should be used for parallel components?
 USER_PROCESS_SKIMS <- TRUE # Should the cv model process skims (if FALSE will if possible skip skim processing and use an existing processed skims file if present)
 USER_RUN_MODE <- "Application" # What type of run is being done? 
