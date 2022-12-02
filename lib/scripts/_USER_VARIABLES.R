5d654bac1e3857172b5077fbe2b15ea2bda51dcb
Colin Smith <colin76@gmail.com>
2022-12-02 10:33:00 -0500

    cleaning up some global scripts

--- lib/scripts/_USER_VARIABLES.R
+++ lib/scripts/_USER_VARIABLES.R
@@ -3,4 +3,5 @@ USER_PROCESSOR_CORES <- 4L # How many processors should be used for parallel com
 USER_PROCESS_SKIMS <- TRUE # Should the cv model process skims (if FALSE will if possible skip skim processing and use an existing processed skims file if present)
 USER_RUN_MODE <- "Application" # What type of run is being done? 
                                # Options are "Application" or "Calibration" 
-                               # where calibration triggers certain model components to run iterative adjustments
+                               # where calibration setting is used by calibration control scripts to pull 
+                               # results from single model components when running the model iteratively
--- run_cmap_csvm.R
+++ run_cmap_csvm.R
@@ -2,19 +2,23 @@
 # Title:             CMAP Commercial Services Vehicle Model
 # Project:           Commercial Services Vehicle Survey and Model
 # Description:       Simulation of commercial services vehicles in the CMAP region
-# Date:              6-18-2022
+# Date:              12-02-2022
 # Author:            Resource Systems Group, Inc.
 # Copyright:         Copyright 2022 RSG, Inc. - All rights reserved.
 ##############################################################################################
 
 # 1. Specific scenario arguments from the command line:
-#    - Scenario name (defaults to base)
-#    - Scenario year (defaults to 2015)
-#    - Run Firm Synthesis component (defaults to TRUE)
-#    - Run CVTM component           (defaults to TRUE)
-#    - Run Trip Tables component    (defaults to TRUE)
-#    - Run Dashboard component      (defaults to TRUE)
+#    1 - Scenario name (folder name for scenario, defaults to "base")
+#    2 - Scenario year (year, defaults to 2019)
+#    3 - Run Firm Synthesis component (TRUE or FALSE, defaults to TRUE)
+#    4 - Run CVTM component           (TRUE or FALSE, defaults to TRUE)
+#    5 - Run Trip Tables component    (TRUE or FALSE, defaults to TRUE)
+#    6 - Run Dashboard component      (TRUE or FALSE, defaults to TRUE)
+#    7 - Reference Scenario name for spreadsheet summary (folder name for scenario, defaults to "base")
+#    8 - Reference Scenario year for spreadsheet summary (year, defaults to 2019)
+
 SYSTEM_COMMAND_ARGS <- commandArgs(TRUE)
 
 # 2. Run the application
+
 source(file.path("lib", "scripts", "__Master.R"))
