67966576b6a1f38cdf16f1a0f09d0532b872d6e3
Colin Smith <colin76@gmail.com>
2022-12-16 15:58:47 -0500

    full set of updated model parameters after complete calibration run including distance adjustments #39 #40

--- dev/calibration_control.R
+++ dev/calibration_control.R
@@ -149,8 +149,8 @@ for(model_step_num in 1:length(models)){
   # Use the repeated process: load outputs from previous step, run step, save results
   # Loop over the submodels
   
-  #####for(submodel_num in 1:length(model_step_submodels)){
-  for(submodel_num in 3:length(model_step_submodels)){
+  for(submodel_num in 1:length(model_step_submodels)){
+  #for(submodel_num in 3:length(model_step_submodels)){
     
     #submodel_num <- 3
     # Name to use for submodel files
