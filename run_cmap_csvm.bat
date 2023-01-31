bf8de91d7a5f2d983ff6ec093aabb5ef967d3769
Colin Smith <colin76@gmail.com>
2023-01-31 07:49:05 -0500

    correcting base year in batch file to 2019

--- run_cmap_csvm.bat
+++ run_cmap_csvm.bat
@@ -3,12 +3,12 @@
 :: Variable definitions
 :: Scenario and Year
 set scenarioname="base"
-set scenarioyear=2017
+set scenarioyear=2019
 
 :: Reference scenario and year to use for scenario comparison
 :: Note: this scenario must have already been run
 set reference="base"
-set referenceyear=2017
+set referenceyear=2019
 
 :: Steps of the model to run (TRUE or FALSE, use upper case)
 set runfirmsyn="TRUE"
@@ -18,7 +18,7 @@ set rundashboard="TRUE"
 
 :: For reference scenario, if this is the base scenario change to validation
 if [%scenarioname%]==["base"] set reference="Validation"
-if [%scenarioname%]==["base"] set referenceyear=2017
+if [%scenarioname%]==["base"] set referenceyear=2019
 
 :: Run CSVM Model For Selected Scenario and Components
 Rscript run_cmap_csvm.R %scenarioname% %scenarioyear% %runfirmsyn% %runcvtm% %runttexp% %rundashboard% %reference% %referenceyear% >run_cmap_csvm_log.txt 2>&1
